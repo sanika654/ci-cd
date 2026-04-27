c7a69b9492f11e7918ac1b25ab653eb4b82d6b05
sanikarana <sanikarana>
2026-04-27 10:19:02 +0530

    second time

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-throw nwe error("My first CI/CD")
+function add(a,b){
+    return a+b
+}
+
+module.exports = { add };
---
+++ test.js
@@ -0,0 +1,16 @@
+// test.js
+const { add } = require('./app');
+
+function runTests() {
+  if (add(2, 3) !== 5) {
+    throw new Error("Test Failed: 2 + 3 should be 5");
+  }
+
+  if (add(-1, 1) !== 0) {
+    throw new Error("Test Failed: -1 + 1 should be 0");
+  }
+
+  console.log("All tests passed ✅");
+}
+
+runTests();
